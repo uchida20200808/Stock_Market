76a3da2b54218b42ab80298ff420d87f54414be2
Uchy <72658923+uchida20200808@users.noreply.github.com>
2020-10-16 09:51:25 +0900

    Update 2Draw_technical_chart.py

--- 2Draw_technical_chart.py
+++ 2Draw_technical_chart.py
@@ -7,20 +7,24 @@ import warnings
 warnings.simplefilter('ignore')
 
 def company_stock(start, end, company_code):
+    #select a datasorce properly.
     df = data.DataReader(company_code, 'yahoo')
     df = df[(df.index>=start) & (df.index<=end)]
     
     date = df.index
+    #check a definition of price.Here is Adj close.
     close = df['Adj Close']
     
-            
+    #check data spans.
     span01 = 5
     span02 = 25
     span03 = 50
     
+    #make lines of mean.
     df['sma01'] = close.rolling(window = span01).mean()
     df['sma02'] = close.rolling(window = span02).mean()
     df['sma03'] = close.rolling(window = span03).mean()
+    #make MACD signals.
     df['macd'], df['macdsignal'], df['macdhist'] = ta.MACD(close, fastperiod=12, slowperiod=26, signalperiod=9)
     df['RSI'] = ta.RSI(close, timeperiod=span02)
     df["upper"], df["middle"], df["lower"] = ta.BBANDS(close, timeperiod=span02, nbdevup=2, nbdevdn=2, matype=0)
@@ -28,26 +32,31 @@ def company_stock(start, end, company_code):
     plt.figure(figsize=(30, 15))
     plt.subplot(5, 1, 1)
     plt.plot(date, close, label = 'Close', color='#99b898')
+    #make mean lines.
     plt.plot(date, df['sma01'], label='sma01', color='#e84a5f')
     plt.plot(date, df['sma02'], label='sma02', color='#ff847c')
     plt.plot(date, df['sma03'], label='sma03', color='#feceab')
     plt.legend()
     
+    #make volume bar.
     plt.subplot(5, 1, 2)
     plt.bar(date, df['Volume'], label='Volume', color='grey')
     plt.legend()
     
+    #make MACD hist chart.
     plt.subplot(5, 1, 3)
     plt.fill_between(date, df['macdhist'], label='MACD_hist', color='grey', alpha=0.5)
     plt.hlines(0, start, end, "grey", linestyles="dashed")
     plt.legend()
     
+    #make RSI chart.
     plt.subplot(5, 1, 4)
     plt.plot(date, df['RSI'], label='RSI', color="grey")
     plt.ylim(0, 100)
     plt.hlines([30, 50, 70], start, end, "grey", linestyles="dashed")
     plt.legend()
     
+    #make Bollinger band.
     plt.subplot(5, 1, 5)
     plt.plot(date, close, label='Close', color='#99b898')
     plt.fill_between(date, df["upper"], df["lower"], color="grey", alpha=0.3)
@@ -60,10 +69,11 @@ def company_stock(start, end, company_code):
     
     plt.show()
     
-    
+#select definitions of start, end.
 start = '2019-10-01'
 end = '2020-10-01'
 
+#select code_name which you'd like to analyze the trend. 
 company_stock(start, end, 'BTC-JPY')
 
 
